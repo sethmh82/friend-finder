c3f8205196e09b4d20afe472cb4928a9b4292354
sethmh82 <seth@statisticbrain.com>
2017-06-18 21:46:19 -0700

    apiRoutes update

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,10 @@
+// DEPENDENCIES
+var express = require('express');
+var route = express.Router();
+
+// EXPRESS
+var path = require('path');
+var friends = './app/data/friends.js';
+
+
+module.exports = route;
